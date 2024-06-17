c3a759b6b392d58b495a0d649f8fc20640d4b089
Manuel Rubio <manuel@altenwald.com>
2024-06-17 12:32:10 +0200

    favicon

--- apps/conta_web/lib/conta_web.ex
+++ apps/conta_web/lib/conta_web.ex
@@ -17,7 +17,7 @@ defmodule ContaWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon robots.txt)
 
   def router do
     quote do
--- apps/conta_web/lib/conta_web/components/layouts.ex
+++ apps/conta_web/lib/conta_web/components/layouts.ex
@@ -2,4 +2,20 @@ defmodule ContaWeb.Layouts do
   use ContaWeb, :html
 
   embed_templates "layouts/*"
+
+  def favicon(assigns) do
+    ~H"""
+    <link rel="apple-touch-icon" sizes="180x180" href={~p"/favicon/apple-touch-icon.png"} />
+    <link rel="icon" type="image/png" sizes="32x32" href={~p"/favicon/favicon-32x32.png"} />
+    <link rel="icon" type="image/png" sizes="16x16" href={~p"/favicon/favicon-16x16.png"} />
+    <link rel="manifest" href={~p"/favicon/site.webmanifest"} />
+    <link rel="mask-icon" href={~p"/favicon/safari-pinned-tab.svg"} color="#9e7802" />
+    <link rel="shortcut icon" href={~p"/favicon/favicon.ico"} />
+    <meta name="apple-mobile-web-app-title" content="Conta" />
+    <meta name="application-name" content="Conta" />
+    <meta name="msapplication-TileColor" content="#ffc40d" />
+    <meta name="msapplication-config" content={~p"/favicon/browserconfig.xml"} />
+    <meta name="theme-color" content="#ffffff" />
+    """
+  end
 end
