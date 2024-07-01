76b7ee7a2b97a8f13aac57d7fa43d65ec57d7322
Manuel Rubio <manuel@altenwald.com>
2024-07-01 11:32:52 +0200

    0.2.6 - bump version

--- apps/conta/mix.exs
+++ apps/conta/mix.exs
@@ -4,7 +4,7 @@ defmodule Conta.MixProject do
   def project do
     [
       app: :conta,
-      version: "0.2.3",
+      version: "0.2.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
