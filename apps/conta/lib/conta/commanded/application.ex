64be79a9897a0e80c0f78837510b7acbbf052750
Manuel Rubio <manuel@altenwald.com>
2024-07-03 00:18:12 +0200

    put longer timeout for avoiding aggregator crash because timeout

--- apps/conta/lib/conta/commanded/application.ex
+++ apps/conta/lib/conta/commanded/application.ex
@@ -1,7 +1,8 @@
 defmodule Conta.Commanded.Application do
   use Commanded.Application,
     otp_app: :conta,
-    event_store: Application.compile_env!(:conta, :event_store)
+    event_store: Application.compile_env!(:conta, :event_store),
+    default_dispatch_opts: [timeout: 30_000]
 
   router(Conta.Commanded.Router)
 end
