f7a50ba7b114f73d63b62a2736d84fc1027e9457
Manuel Rubio <manuel@altenwald.com>
2026-07-13 21:58:30 +0200

    feat: register Reconciliation aggregate in the Commanded router

--- apps/conta/lib/conta/commanded/router.ex
+++ apps/conta/lib/conta/commanded/router.ex
@@ -4,14 +4,20 @@ defmodule Conta.Commanded.Router do
   alias Conta.Aggregate.Automator
   alias Conta.Aggregate.Company
   alias Conta.Aggregate.Ledger
+  alias Conta.Aggregate.Reconciliation
 
+  alias Conta.Command.ImportMovements
+  alias Conta.Command.MarkMovementTransacted
   alias Conta.Command.RemoveAccount
   alias Conta.Command.RemoveAccountTransaction
   alias Conta.Command.RemoveContact
   alias Conta.Command.RemoveExpense
   alias Conta.Command.RemoveFilter
   alias Conta.Command.RemoveInvoice
+  alias Conta.Command.RemoveMatchRule
+  alias Conta.Command.RemoveMovement
   alias Conta.Command.RemoveShortcut
+  alias Conta.Command.ReorderMatchRules
   alias Conta.Command.SetAccount
   alias Conta.Command.SetAccountTransaction
   alias Conta.Command.SetCompany
@@ -19,9 +25,11 @@ defmodule Conta.Commanded.Router do
   alias Conta.Command.SetExpense
   alias Conta.Command.SetFilter
   alias Conta.Command.SetInvoice
+  alias Conta.Command.SetMatchRule
   alias Conta.Command.SetPaymentMethod
   alias Conta.Command.SetShortcut
   alias Conta.Command.SetTemplate
+  alias Conta.Command.UpdateMovement
 
   identify(Automator, by: :automator)
 
@@ -46,4 +54,14 @@ defmodule Conta.Commanded.Router do
   dispatch(SetInvoice, to: Company, identity: :nif)
   dispatch(SetPaymentMethod, to: Company, identity: :nif)
   dispatch(SetTemplate, to: Company, identity: :nif)
+
+  identify(Reconciliation, by: :reconciliation)
+
+  dispatch(ImportMovements, to: Reconciliation)
+  dispatch(MarkMovementTransacted, to: Reconciliation)
+  dispatch(RemoveMatchRule, to: Reconciliation)
+  dispatch(RemoveMovement, to: Reconciliation)
+  dispatch(ReorderMatchRules, to: Reconciliation)
+  dispatch(SetMatchRule, to: Reconciliation)
+  dispatch(UpdateMovement, to: Reconciliation)
 end
