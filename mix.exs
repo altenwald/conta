1fb040dc6934b0fbee09fabd3042135dbb201852
Manuel Rubio <manuel@altenwald.com>
2026-08-20 21:53:31 +0200

    Bump version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Conta.Umbrella.MixProject do
     [
       name: :conta,
       apps_path: "apps",
-      version: "0.3.0",
+      version: "0.3.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
