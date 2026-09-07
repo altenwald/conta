e599c5ff89b213a032fd8111aef6e388a4c12909
Manuel Rubio <manuel@altenwald.com>
2026-09-07 20:41:27 +0200

    Upgrade dependencies and bump 0.4.5

--- apps/conta/mix.exs
+++ apps/conta/mix.exs
@@ -4,7 +4,7 @@ defmodule Conta.MixProject do
   def project do
     [
       app: :conta,
-      version: "0.4.4",
+      version: "0.4.5",
       build_path: "../../_build",
 
       config_path: "../../config/config.exs",
